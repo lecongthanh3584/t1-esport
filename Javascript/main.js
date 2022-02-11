4018fe79246eafd7e1fee36b432634d27d50ab55
DESKTOP-NEA8RT1\Admin <thanh232002@gmail.com>
2022-02-11 12:06:39 +0700

    update

--- Javascript/main.js
+++ Javascript/main.js
@@ -8,15 +8,23 @@ window.onscroll = () =>{
     navbar.classList.remove('open');
 }
 
-var myIndex = 0;
-carousel();
-function carousel() {
-    const x = document.getElementsByClassName("js-slider");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 4000); 
-}
+// var myIndex = 0;  /**Đây là code chạy slider không vuốt được */
+// carousel();
+// function carousel() {
+//     const x = document.getElementsByClassName("js-slider");
+//     for (let i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";  
+//     }
+//     myIndex++;
+//     if (myIndex > x.length) {myIndex = 1}    
+//     x[myIndex-1].style.display = "block";  
+//     setTimeout(carousel, 4000); 
+// }
+
+$('.slider-contain').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
